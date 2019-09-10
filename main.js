73c7668b9df7399ced24bd39f0841880e84aaa9c
plOnion <damian.lukasiewicz@gmail.com>
2019-09-10 14:17:06 +0200

    nav handleClick fix

--- main.js
+++ main.js
@@ -1,41 +1,44 @@
 const hamburger = document.querySelector('.hamburger');
-        const nav = document.querySelector('.navigation');
+const nav = document.querySelector('.navigation');
+const menuItem = [...document.querySelectorAll(".cool-link")];
 
-        const handleClick = () => {
-            hamburger.classList.toggle('hamburger--active');
-            hamburger.setAttribute('aria-expanded', hamburger.classList.contains('hamburger--active'));
-            nav.classList.toggle('navigation--active');
-        }
+const handleClick = () => {
+    hamburger.classList.toggle('hamburger--active');
+    hamburger.setAttribute('aria-expanded', hamburger.classList.contains('hamburger--active'));
+    nav.classList.toggle('navigation--active');
+}
 
-        hamburger.addEventListener('click', handleClick);
+hamburger.addEventListener('click', handleClick);
+menuItem.forEach(item =>{
+    item.addEventListener('click', handleClick)});
 
-        const menu = document.querySelector("nav");
-        const call = [...document.querySelectorAll(".call__link")];
+const menu = document.querySelector("nav");
+const call = [...document.querySelectorAll(".call__link")];
 
-        const handleScroll = () => {
-            if (window.scrollY > 100) {
+const handleScroll = () => {
+    if (window.scrollY > 100) {
 
-                menu.style.position = "fixed"
-            } else {
-                menu.style.position = "relative"
-            }
-        };
+        menu.style.position = "fixed"
+    } else {
+        menu.style.position = "relative"
+    }
+};
 
-        const handleCall = (e) => {
-            if (e.target.textContent !== "tel. 601 611 321") {
-                e.target.textContent = "tel. 601 611 321"
-            } else {
-                e.target.textContent = "ZADZWOŃ"
-            }
-        }
+const handleCall = (e) => {
+    if (e.target.textContent !== "tel. 601 611 321") {
+        e.target.textContent = "tel. 601 611 321"
+    } else {
+        e.target.textContent = "ZADZWOŃ"
+    }
+}
 
-        window.addEventListener("scroll", handleScroll);
-        call.forEach(callBtn => {
-            callBtn.addEventListener("click", handleCall);
-            callBtn.addEventListener("mouseover", handleCall);
-            callBtn.addEventListener("mouseout", handleCall);
-        });
+window.addEventListener("scroll", handleScroll);
+call.forEach(callBtn => {
+    callBtn.addEventListener("click", handleCall);
+    callBtn.addEventListener("mouseover", handleCall);
+    callBtn.addEventListener("mouseout", handleCall);
+});
 
 
-        //             if (window.scrollY) {
-        //   window.scroll(0, 0);  // reset the scroll position to the top left of the document.
+//             if (window.scrollY) {
+//   window.scroll(0, 0);  // reset the scroll position to the top left of the document.
